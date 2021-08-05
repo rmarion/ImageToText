7d084c8d026de26447c3b8b8f820442246249144
rmarion-dev <development@rmarion.com>
2021-08-05 00:04:02 -0400

    cleaning up processing, working on scaling

--- imagetotext.py
+++ imagetotext.py
@@ -1,4 +1,5 @@
-from PIL import Image
+import argparse
+from PIL import Image, ImageOps
 
 
 # This will cut off images that aren't a multiple of (2 x 4). If this becomes an issue for some reason, get_character_for_pixels can be extended for partials
@@ -28,31 +29,72 @@ def get_text_for_image(image):
 
 
 def get_character_for_pixels(pixels):
-    threshold = 255 / 2
+    threshold = 75
     byte = 255  # we'll progressively remove the non-matching parts of this with a bytemask to get our matching code
-    
-    if sum(pixels[0, 0]) / 3 > threshold: # ⣾
+
+    if pixels[0, 0] > threshold: # ⣾
         byte ^= 1
 
-    if sum(pixels[0, 1]) / 3 > threshold: # ⣽
+    if pixels[0, 1] > threshold: # ⣽
         byte ^= 2
 
-    if sum(pixels[0, 2]) / 3 > threshold: # ⣻
+    if pixels[0, 2] > threshold: # ⣻
         byte ^= 4
 
-    if sum(pixels[1, 0]) / 3 > threshold: # ⣷
+    if pixels[1, 0] > threshold: # ⣷
         byte ^= 8
 
-    if sum(pixels[1, 1]) / 3 > threshold: # ⣯
+    if pixels[1, 1] > threshold: # ⣯
         byte ^= 16
 
-    if sum(pixels[1, 2]) / 3 > threshold: # ⣟
+    if pixels[1, 2] > threshold: # ⣟
         byte ^= 32
 
-    if sum(pixels[0, 3]) / 3 > threshold: # ⢿
+    if pixels[0, 3] > threshold: # ⢿
         byte ^= 64
 
-    if sum(pixels[1, 3]) / 3 > threshold: # ⡿
+    if pixels[1, 3] > threshold: # ⡿
         byte ^= 128
 
     return chr(0X2800 + byte)  # the braille unicode goes from 0X2800 -> 0X28FF
+
+
+def get_args(argv=None):
+    parser = argparse.ArgumentParser('Converts images to text')
+    parser.add_argument('file', type=str, help='The file to make sarcastic')
+    args = parser.parse_args()
+    return args
+
+
+def load_image(path, max_size):
+    try:
+        max_size = float(max_size)
+        image = Image.open(path)
+        current_size = image.size
+
+        if current_size[0] > current_size[1]:
+            ratio = max_size / current_size[0]
+            new_size = (int(ratio * current_size[0]), int(ratio * current_size[1]))
+        else:
+            ratio = max_size / current_size[1]
+            new_size = (int(ratio * current_size[0]), int(ratio * current_size[1]))
+
+        return ImageOps.grayscale(image.resize(new_size))
+        # return image
+    except Exception as e:
+        print(str(e))
+        return None
+
+
+def main(args):
+    if not args.file:
+        return 'You must provide either a file or phrase. Type sarcastic.py -h for help.'
+    image = load_image(args.file, 128)
+    if not image:
+        return f'Failed to load {args.file}.'
+    return get_text_for_image(image)
+    
+
+if __name__ == '__main__':
+    args = get_args()
+    print(main(args))
